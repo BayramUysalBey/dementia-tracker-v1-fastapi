52da85d57167fc5527f7ae96d979077b10014dd6
BayramUysalBey <bayramuysal@windowslive.com>
2026-05-16 12:46:09 +0300

    feat: integrate NiceGUI, initialize main FastAPI application, and add CI pipeline for automated testing

---
+++ app/api/routers/nicegui.py
@@ -0,0 +1,9 @@
+# from app.services.medication import MedicationService
+# import app.main
+# from nicegui import ui
+
+
+# @ui.page('/debug/meds')
+# def meds_page():
+#     meds = get_all_medication()
+#     ui.table(columns=['Name', 'Dosage'], rows=meds)
--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.api.routers import api_router
 from app.core.settings import settings
 from fastapi.staticfiles import StaticFiles
+from nicegui import ui
+import app.api.routers.nicegui
+
 
 app = FastAPI(
     title="Dementia Tracker V1 API",
@@ -11,6 +14,7 @@ app = FastAPI(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api_router, prefix="/api")
+ui.run_with(app)
 
 @app.get("/")
 async def main():
